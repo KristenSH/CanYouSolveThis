af3009b51e19a09913fbd2b7fcda595da6d6990a
KRISTEN-SKOGHOL\krist <kristen.s.haave@outlook.com>
2019-10-07 15:23:54 +0200

    Created the first level, before merging into AR

---
+++ Can You Solve This/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    //private void OnMouseDown()
+    //{
+    //    if (!GameControl.sucsess)
+    //        transform.Rotate(0f, 0f, 90f);
+    //}
+
+    ////Ball properties
+    //private Rigidbody rBody;
+    //public float speed;
+    //private Animator anim;
+
+    //private float moveForwardBack = 5.0f;
+    //private float moveLeftRight = 300.0f;
+    //public float moveUpDown;
+
+
+    ////Inputs for smartphone
+    //[SerializeField]
+    //float lvAxis;
+    //float lhAxis;
+    //float lt;
+    //float rt;
+    //bool RotateLeft;
+    //bool RotateRight;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    rBody = GetComponent<Rigidbody>();
+    //    anim = GetComponent<Animator>();
+    //    Vector3 pos = transform.localPosition;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //Input function
+
+    //}
+}
